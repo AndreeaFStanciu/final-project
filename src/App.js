cdded7b8cbeac492ffdb83e754ba44b1923dcccf
AndreeaS <andreea_raducu@yahoo.com>
2023-02-28 20:17:58 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}>
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
